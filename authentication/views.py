cfa4945aa005b32ccbe3183b72d4e2e10e54716f
Anasul Firos <anasulfiros7@gmail.com>
2023-09-21 09:22:00 +0530

    working model of Login system

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello')
+    return render(request, "authentication/index.html")
+
+def signup(request):
+
+    if request.method == "POST":
+        username = request.POST['username']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if User.objects.filter(username=username):
+            messages.error(request,"Username is already exist ! . Please try some other username")
+            return redirect('home')
+        
+        if User.objects.filter(email=email):
+            messages.error(request, "Email already registered !")
+            return redirect('home')
+        
+        if len(username)>10:
+            messages.error(request, "Username must be under 10 character")
+
+        if password1 != password2:
+            messages.error(request, "Password doesn't match")
+
+        if not username.isalnum():
+            messages.error(request, "Username must be Alpha-Numeric")
+            return redirect('home')
+
+
+        myuser = User.objects.create_user(username, email, password1)
+        myuser.first_name = firstname
+        myuser.last_name = lastname
+
+        myuser.save()
+
+        messages.success(request, "Your account has been successfully created.")
+
+        return redirect('signin')
+
+    return render(request, "authentication/signup.html")
+
+def signin(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            firstname = user.first_name
+            return render(request, 'authentication/index.html', {'firstname' : firstname})
+        
+        else:
+            messages.error(request, 'Bad credential')
+            return redirect('home')
+
+    return render(request, "authentication/signin.html")
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully")
+    return redirect('home')
